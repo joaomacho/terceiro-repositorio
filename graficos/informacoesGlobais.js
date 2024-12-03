e3cd07c60ebf3676f8ef37cf56671b76705f2435
Joao Guilherme Pires Pessoa <113534290+joaomacho@users.noreply.github.com>
2024-12-03 20:30:33 -0300

    kk

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-onst url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
